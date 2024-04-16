d6d59ede3f505f3db0ea926a591583f7db66689f
あ <2230064@ecc.ac.jp>
2024-04-16 10:54:54 +0900

    カメラ

--- Sources/CameraBehavior.cpp
+++ Sources/CameraBehavior.cpp
@@ -51,10 +51,10 @@ void DebugCameraBehavior::applyMayaControl(GameObject* obj, float elapsedTime)
 		const float moveX = static_cast<float>(input::getCursorDeltaX() * moveSpeed);
 		const float moveY = static_cast<float>(input::getCursorDeltaY() * moveSpeed);
 
-		obj->transform_->position_ +=  camera->rightVec_ * moveX;
-		obj->transform_->position_ += -camera->upVec_	 * moveY;
-		camera->target_ +=  camera->rightVec_ * moveX;
-		camera->target_ += -camera->upVec_	  * moveY;
+		obj->transform_->position_ -=  camera->rightVec_ * moveX;
+		obj->transform_->position_ -= -camera->upVec_	 * moveY;
+		camera->target_ -=  camera->rightVec_ * moveX;
+		camera->target_ -= -camera->upVec_	  * moveY;
 	}
 
 	if (GetAsyncKeyState(VK_MENU) && input::state(input::LMB))
